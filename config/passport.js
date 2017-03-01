4e4c663902a6968afc69dd2d24a4734a0ead4fdd
Yuriy Makov <juramak98@gmail.com>
2017-03-01 13:25:51 +0200

    start add front, backend works fine.

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
-var JwtStrategy = require('passport-jwt').Strategy;
+var JwtStrategy = require('passport-jwt').Strategy,
+ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // load up the user model
 var User = require('../app/models/user');
@@ -6,6 +7,7 @@ var config = require('../config/database'); // get db config file
 
 module.exports = function(passport) {
     var opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
     opts.secretOrKey = config.secret;
     passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
         User.findOne({id: jwt_payload.id}, function(err, user) {
